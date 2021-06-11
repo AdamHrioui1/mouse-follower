6f0115605ecd1162288fef222048b93c7549591b
AdamHrioui1 <essaouira024@gmail.com>
2021-06-10 20:00:31 -0700

    drop

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
-//const follower_container = document.getElementById("follower_container");
-const ball1 = document.getElementById("ball1");
-let tab = [];
+const follower = document.getElementById("follower");
 
-document.addEventListener("mousemove", e => {
+document.addEventListener("mousemove", (e) => {
     e.preventDefault();
-/*    follower_container.style.top = e.clientY + "px";
-    follower_container.style.left = e.clientX + "px";*/
-
-    ball1.style.top = e.clientY + "px";
-    ball1.style.left = e.clientX + "px";
-
-    let obj = {
-        x : e.clientX, 
-        y : e.clientY
-    }
-    tab.push(obj);
-
-    /*for(let i = 0; i < tab.length; i++){
-        let ball = document.createElement("div");
-        ball.classList.add("ball");
-
-        ball.style.top = tab[i].y + "px";
-        ball.style.left = tab[i].x + "px";
-
-        document.body.appendChild(ball);
-        
-        setTimeout(() => {
-            tab.splice(0, tab.length-1)
-            document.body.removeChild(ball)
-        }, 70)
-    }*/
+    follower.style.top = e.clientY + "px";
+    follower.style.left = e.clientX + "px";
 })
